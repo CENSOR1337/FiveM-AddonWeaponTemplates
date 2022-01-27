0610d85c104608bbe0db8b486b50cb44f50d807e
MeteePC\Metee <viixxivvimm@gmail.com>
2022-01-27 19:31:46 +0700

    Added Machete, Bottle

---
+++ WEAPON_BOTTLE/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+games {
+    "gta5"
+}
+
+files {
+    "**/weaponcomponents.meta",
+    "**/weaponarchetypes.meta",
+    "**/pedpersonality.meta",
+    "**/weaponanimations.meta",
+    "**/weapon.meta"
+}
+
+data_file "WEAPONCOMPONENTSINFO_FILE" "**/weaponcomponents.meta"
+data_file "WEAPON_METADATA_FILE" "**/weaponarchetypes.meta"
+data_file "PED_PERSONALITY_FILE" "**/pedpersonality.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "**/weaponanimations.meta"
+data_file "WEAPONINFO_FILE" "**/weapon.meta"
