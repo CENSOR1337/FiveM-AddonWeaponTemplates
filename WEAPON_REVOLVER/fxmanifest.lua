bc1913fb5f12e729fb69ff65d5693d2b85e6bb76
MeteePC\Metee <viixxivvimm@gmail.com>
2022-03-27 16:17:38 +0700

    Update Revolver

--- WEAPON_REVOLVER/fxmanifest.lua
+++ WEAPON_REVOLVER/fxmanifest.lua
@@ -8,4 +8,6 @@ files {
 }
 
 data_file "WEAPON_METADATA_FILE" "**/weaponarchetypes.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "**/weaponanimations.meta"
+data_file "PED_PERSONALITY_FILE" "**/pedpersonality.meta"
 data_file "WEAPONINFO_FILE" "**/weapon.meta"
