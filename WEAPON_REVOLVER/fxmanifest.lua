7872451c2b16f3b45b68e1ee9b7b53c2aa3242be
MeteePC\Metee <viixxivvimm@gmail.com>
2022-03-27 13:56:54 +0700

    Added revolver

---
+++ WEAPON_REVOLVER/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+games {
+    "gta5"
+}
+
+files {
+    "**/*.meta"
+}
+
+data_file "WEAPON_METADATA_FILE" "**/weaponarchetypes.meta"
+data_file "WEAPONINFO_FILE" "**/weapon.meta"
