7ecf26be4c9a6572e825d1c968287ef52c9e1f22
CENSOR_1337 <viixxivvimm@gmail.com>
2022-09-15 07:58:24 +0700

    finalize WEAPON_WRENCH

---
+++ WEAPON_WRENCH/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+games {
+    "gta5"
+}
+
+files {
+    "**/*.meta"
+}
+
+data_file "WEAPON_METADATA_FILE" "**/weaponarchetypes.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "**/weaponanimations.meta"
+data_file "PED_PERSONALITY_FILE" "**/pedpersonality.meta"
+data_file "WEAPONINFO_FILE" "**/weapon.meta"
